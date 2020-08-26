38a45c4b7061db6bd9d1c95e2e22a93fd54b7f9c
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-08-26 21:28:09 +0200

    Extend coordinator pg

--- coordinator.playground/Contents.swift
+++ coordinator.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
 
+// http://merowing.info/2016/01/improve-your-ios-architecture-with-flowcontrollers/
 // https://www.hackingwithswift.com/articles/71/how-to-use-the-coordinator-pattern-in-ios-apps
 // https://dmtopolog.com/thoughts-on-ios-architecture
-
