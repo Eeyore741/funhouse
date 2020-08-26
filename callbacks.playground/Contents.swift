b2fc28941fbde29f1a710acbd688b3017f0eaee2
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-08-26 20:37:49 +0200

    Add callbacks pg

---
+++ callbacks.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+// https://nalexn.github.io/callbacks-part-1-delegation-notificationcenter-kvo
+// https://nalexn.github.io/callbacks-part-2-closure-target-action-responder-chain
+// https://nalexn.github.io/callbacks-part-3-promise-event-stream
+
