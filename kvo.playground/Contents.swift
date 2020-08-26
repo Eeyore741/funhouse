2cd051cdc668b5f4ee437d99829cb474a740b14d
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-08-26 20:38:48 +0200

    Update kvo pg

--- kvo.playground/Contents.swift
+++ kvo.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+// https://nalexn.github.io/kvo-guide-for-key-value-observing/
+// https://developer.apple.com/documentation/swift/cocoa_design_patterns/using_key-value_observing_in_swift
+
 /// Type need to inherit from `NSObject` to utilize `NSKeyValueObserving` protocol
 final class Foo: NSObject {
     @objc dynamic var bar: Int = 0 // property have to be exposed to objc runtime and be observable
@@ -14,3 +17,5 @@ observation = foo.observe(\.bar, options: NSKeyValueObservingOptions.new) { (foo
 
 foo.bar = 0
 foo.bar = 1
+
+observation?.invalidate()
