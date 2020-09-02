9e60457ebe9c788ce2982ce4c984ce7c9d892061
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-09-02 17:38:41 +0200

    Add aytolayout hugging and compression

---
+++ hugging-compression.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+// https://slicode.com/content-hugging-and-compression-resistance-in-auto-layout
+// https://krakendev.io/blog/autolayout-magic-like-harry-potter-but-real
+// https://devtut.github.io/ios/content-hugging-content-compression-in-autolayout.html
+// https://www.iosinsight.com/content-hugging-and-compression-resistance-explained
+// https://stackoverflow.com/questions/15850417/cocoa-autolayout-content-hugging-vs-content-compression-resistance-priority
+// https://medium.com/@abhimuralidharan/ios-content-hugging-and-content-compression-resistance-priorities-476fb5828ef
