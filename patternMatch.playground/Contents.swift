0163019579e54906a23ceb48e9a018a71ee6a5c8
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-09-08 19:21:50 +0200

    Extend pattern matching

--- patternMatch.playground/Contents.swift
+++ patternMatch.playground/Contents.swift
@@ -1,3 +1,7 @@
 import Foundation
 
-// https://alisoftware.github.io/swift/pattern-matching/2016/03/27/pattern-matching-1/
+// https://alisoftware.github.io/swift/pattern-matching/2016/03/27/pattern-matching-1
+// https://andybargh.com/pattern-matching-in-swift
+// https://www.bignerdranch.com/blog/pro-pattern-matching-in-swift
+// https://www.swiftbysundell.com/articles/pattern-matching-in-swift
+
