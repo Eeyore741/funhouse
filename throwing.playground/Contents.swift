5c674f48f77e9386f6157f0d26a3f26302d7d0c9
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-09-13 19:18:04 +0200

    Add throwing

---
+++ throwing.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+//https://mokriya.com/blog/throwing-arithmetic-operators-in-swift
+//https://learnappmaking.com/error-handling-swift-do-try-catch
+//https://www.swiftbysundell.com/tips/implementing-throwing-protocol-functions-as-non-throwing
+//https://www.tutlane.com/tutorial/swift/swift-throw-statement
+//https://andybargh.com/error-handling-in-swift
+//https://codewithchris.com/swift-try-catch
+//https://www.avanderlee.com/swift/try-catch-throw-error-handling
+//https://www.donnywals.com/working-with-throwing-functions-in-swift
+//https://www.hackingwithswift.com/example-code/language/what-is-a-throwing-function
+//https://www.hackingwithswift.com/sixty/5/8/writing-throwing-functions
+//https://theswiftdev.com/how-to-use-the-result-type-to-handle-errors-in-swift
+//https://nshipster.com/optional-throws-result-async-await
+
