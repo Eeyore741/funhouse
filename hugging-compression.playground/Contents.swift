332552285212c655bf93df9532c263a64ab2699f
Vitalii Kuznetsov <vitalii.kuznetsov@storytel.com>
2020-12-29 21:36:50 +0100

    Add hugging-compression

--- hugging-compression.playground/Contents.swift
+++ hugging-compression.playground/Contents.swift
@@ -1,9 +1,13 @@
 
 import UIKit
 
-// https://slicode.com/content-hugging-and-compression-resistance-in-auto-layout
-// https://krakendev.io/blog/autolayout-magic-like-harry-potter-but-real
-// https://devtut.github.io/ios/content-hugging-content-compression-in-autolayout.html
-// https://www.iosinsight.com/content-hugging-and-compression-resistance-explained
-// https://stackoverflow.com/questions/15850417/cocoa-autolayout-content-hugging-vs-content-compression-resistance-priority
-// https://medium.com/@abhimuralidharan/ios-content-hugging-and-content-compression-resistance-priorities-476fb5828ef
+//https://slicode.com/content-hugging-and-compression-resistance-in-auto-layout
+//https://krakendev.io/blog/autolayout-magic-like-harry-potter-but-real
+//https://devtut.github.io/ios/content-hugging-content-compression-in-autolayout.html
+//https://www.iosinsight.com/content-hugging-and-compression-resistance-explained
+//https://stackoverflow.com/questions/15850417/cocoa-autolayout-content-hugging-vs-content-compression-resistance-priority
+//https://medium.com/@abhimuralidharan/ios-content-hugging-and-content-compression-resistance-priorities-476fb5828ef
+
+//https://medium.com/@hassanahmedkhan/the-ultimate-guide-to-uilayoutguide-d582d6981417
+//https://medium.com/the-traveled-ios-developers-guide/uilayoutguide-6b3b552b1890
+//https://developer.apple.com/documentation/uikit/uilayoutguide
